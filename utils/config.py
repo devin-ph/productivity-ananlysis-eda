0d5ec955d259cfcbe8da759bcb4ed30aea07642a
d e v i n <devinphmhoag@gmail.com>
2025-12-08 17:34:59 +0700

    Delete Config

--- utils/config.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-plt.style.use('ggplot')
-sns.set_theme(style='whitegrid')
-
-def load_data():
-    return pd.read_csv("../data/productivity_raw.csv")
